e6a9da1464b78ca2d664be519a8e6fb91d8121e7
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2021-06-29 17:33:04 +0530

    correct .pkg.sh path

--- .pkg.sh
+++ .pkg.sh
@@ -2,7 +2,6 @@ sudo apt install vim && echo package vim installed
 sudo apt install vlc && echo package vlc installed
 sudo apt install shellcheck && echo package shellcheck installed
 sudo apt install r-base && echo package r-base installed
-sudo apt install r-base && echo package r-base installed
 sudo apt install tldr && echo package tldr installed
 sudo apt install iftop && echo package iftop installed
 sudo apt install iotop && echo package iotop installed
