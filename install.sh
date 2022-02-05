800bc76a1e204e90c56cc41cacdd16138311b310
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2022-02-05 12:13:25 +0530

    beautify install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
-# First the business of the shell. 
+#!/bin/bash
+
+# First the business of zsh. 
 # ---------------------------------
+# first i need to install a check if zsh exists or no
 sudo apt install zsh							# Install zsh
 chsh -s /usr/bin/zsh && echo "zsh made the default shell"		# First change the default shell to zsh
 
@@ -7,25 +10,26 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com\
 /ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo \
 "oh-my-zsh installed"							# install oh-my-zsh
 
-if [[ -f ~/.zshrc ]]
-then
-	do mv ~/.zshrc ~/.zshrc.old && echo \
-		".zshrc moved to .zshrc.old"
-done
+# check if zshrc exists and move it
+if [[ -f ~/.zshrc ]]; then
+	mv ~/.zshrc ~/.zshrc.old && \
+	echo ".zshrc moved to .zshrc.old"
+fi
 
-ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc && echo "soft \                    
-link to zshrc created"                                                  # create symlink to zshrc
+ # create symlink to zshrc
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc && \
+echo "soft link to zshrc created"                                      
 
 # check jose's file for inspiration
 #ln -sf ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh && echo\
 #"soft link to .p10k.zsh created"
 #--------------------------------------
 
-ln -sf ~/.dotfiles/vim/.vimrc ~/.vimrc && echo "soft \
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc && echo "soft \
 link to vimrc created"						 	# create link to the vimrc
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig && echo "soft \
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig && echo "soft \
 link to gitconfig created"  						# create link to the gitconfig
-ln -sf ~/.dotfiles/.matplotlibrc ~/.config/matplotlib/\
+ln -sf ~/dotfiles/matplotlibrc ~/.config/matplotlib/\
 .matplotlibrc && echo "soft link to matplotlibrc created" 		# create link to matplotlibrc
 
 # Additional information
