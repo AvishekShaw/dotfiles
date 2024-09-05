d0a1899b6a368c5d76cab5c04e6b2e9015eeaddc
Avishek Shaw <egregious@Avisheks-MacBook-Air.local>
2024-09-05 18:05:47 +0530

    comments after commands

--- sync_notes.sh
+++ sync_notes.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+dashes() {
+    local length=$1
+    local dashes=$(printf "%*s" $length "")
+    echo "${dashes// /-}"
+}
+
 # Determine the environment based on the output of whoami
 if [ "$(whoami)" = "egregious" ]; then
     # macOS
@@ -22,12 +28,15 @@ find . -type f \( -name "*.md" -o -name "*.png" -o -name "*.pdf" \) -print0 | xa
 
 # Add deleted files
 "$GIT_CMD" ls-files --deleted -z | xargs -0 "$GIT_CMD" add
+dashes 50
 
 # Commit changes
-"$GIT_CMD" commit -m "Sync on $(date)"
+"$GIT_CMD" commit -m "Sync on $(date)" && echo "Changes committed"
+dashes 50
 
 # Pull changes from remote
-"$GIT_CMD" pull origin master
-
+"$GIT_CMD" pull origin master && echo "Changes pulled"
+dashes 50
 # Push changes to remote
-"$GIT_CMD" push origin master
+"$GIT_CMD" push origin master && echo "Changes pushed"
+dashes 50
